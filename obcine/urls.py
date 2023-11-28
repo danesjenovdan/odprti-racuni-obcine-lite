835c3f876db446d8a24852e0ee052487618f8564
LepkoQQ <lepko.san@gmail.com>
2023-11-28 14:38:15 +0100

    change subpage url and add redirect for old one

--- obcine/urls.py
+++ obcine/urls.py
@@ -21,6 +21,8 @@ urlpatterns = [
     path("prikaz-sredstev-skozi-cas/<int:municipality_id>/<int:year_id>/", views.OldUrlRedirectView.as_view(pattern_name="comparison_over_time")),
     path("prikaz-sredstev-skozi-cas/<int:municipality_id>/<int:year_id>/table/", views.OldUrlRedirectView.as_view(pattern_name="comparison_over_time_table")),
     path("prikaz-sredstev-skozi-cas/<int:municipality_id>/<int:year_id>/chart_data/", views.OldUrlRedirectView.as_view(pattern_name="comparison_over_time_chart_data")),
+    path("<str:municipality_slug>/prikaz-sredstev-skozi-cas/", RedirectView.as_view(url="/%(municipality_slug)s/primerjava-skozi-cas/", query_string=True)),
+    path("<str:municipality_slug>/prikaz-sredstev-skozi-cas/<path:path>", RedirectView.as_view(url="/%(municipality_slug)s/primerjava-skozi-cas/%(path)s", query_string=True)),
 
     # Pages
     path("<str:municipality_slug>/pregled/", views.overview, name="overview"),
@@ -30,8 +32,10 @@ urlpatterns = [
     path("<str:municipality_slug>/razrez-sredstev/<str:year_slug>/", views.cut_of_funds, name="cut_of_funds"),
     path("<str:municipality_slug>/razrez-sredstev/<str:year_slug>/table/", views.cut_of_funds_table, name="cut_of_funds_table"),
 
-    path("<str:municipality_slug>/prikaz-sredstev-skozi-cas/", views.comparison_over_time, name="comparison_over_time"),
-    path("<str:municipality_slug>/prikaz-sredstev-skozi-cas/<str:year_slug>/", views.comparison_over_time, name="comparison_over_time"),
-    path("<str:municipality_slug>/prikaz-sredstev-skozi-cas/<str:year_slug>/table/", views.comparison_over_time_table, name="comparison_over_time_table"),
-    path("<str:municipality_slug>/prikaz-sredstev-skozi-cas/<str:year_slug>/chart_data/", views.comparison_over_time_chart_data, name="comparison_over_time_chart_data"),
+    path("<str:municipality_slug>/primerjava-skozi-cas/", views.comparison_over_time, name="comparison_over_time"),
+    path("<str:municipality_slug>/primerjava-skozi-cas/<str:year_slug>/", views.comparison_over_time, name="comparison_over_time"),
+    path("<str:municipality_slug>/primerjava-skozi-cas/<str:year_slug>/table/", views.comparison_over_time_table, name="comparison_over_time_table"),
+    path("<str:municipality_slug>/primerjava-skozi-cas/<str:year_slug>/chart_data/", views.comparison_over_time_chart_data, name="comparison_over_time_chart_data"),
+
+
 ]
